d3b5b73d10c1961434c25ba79a72e806632f4d01
Macintosh HD <macintoshhd@Macintoshs-MacBook-Air.local>
2019-03-28 19:21:55 +0700

    add NoSql

---
+++ NoSql.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Helper;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
+
+class NoSql
+{
+    public function save($fileName, $json, $menit = 1)
+    {
+        $genesis = Carbon::now();
+        $next_block = $genesis->addMinutes($menit)->timestamp;
+
+        $data = collect($json)->put('next_refresh', $next_block);
+        $data = $data->toJson();
+
+        Storage::disk('public')->put('NoSql/'.$fileName.'.json', $data);
+    }
+
+    public function reader($fileName)
+    {
+        try{
+            $data = Storage::disk('public')->get('/NoSql/'.$fileName.'.json');
+            $data = \json_decode($data, true);
+            
+            if(empty($data)){
+                $reponse = [
+                    'refresh' => true,
+                    'data' => []
+                ];
+            } else {
+                if($data['next_refresh'] > Carbon::now()->timestamp){
+                    $reponse = [
+                        'refresh' => false,
+                        'data' => collect($data)->except(['next_refresh'])
+                    ];
+                } else {
+                    $reponse = [
+                        'refresh' => true,
+                        'data' => []
+                    ];
+                }
+            }
+        } catch(\Exception $e){
+            $reponse = [
+                'refresh' => true,
+                'data' => $e->getMessage()
+            ];
+        }
+        return $reponse;
+    }
+}
